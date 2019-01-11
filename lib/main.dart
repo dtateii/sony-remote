a72241048219cd4ea624adf79a2891d4d5f34eb6
Dave Tate <tate.david.j@gmail.com>
2019-01-10 22:39:59 -0500

    Network: Add basic connectivity check

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 void main() => runApp(MyApp());
+
+var receiverAPI = 'http://192.168.86.243:10000/sony/'; // avContent';
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,7 +34,7 @@ class CustomTextStyle {
   static TextStyle label (BuildContext context) {
     return Theme.of(context).textTheme.display4.copyWith(
       fontWeight: FontWeight.normal,
-      fontSize: 14.0,
+      fontSize: 28.0,
       color: Colors.white30,
     );
   }
@@ -35,32 +42,31 @@ class CustomTextStyle {
   static TextStyle button (BuildContext context) {
     return Theme.of(context).textTheme.display4.copyWith(
       fontWeight: FontWeight.w100,
-      fontSize: 34.0,
+      fontSize: 46.0,
       color: Colors.white,
     );
   }
 
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => new Scaffold(
-    body: new Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        new Card(
-          child: new Container(
-
-            child: new Text(
-              'NJI Media',
-              style: CustomTextStyle.button(context),
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
-}
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) => new Scaffold(
+//     body: new Column(
+//       crossAxisAlignment: CrossAxisAlignment.stretch,
+//       children: [
+//         new Card(
+//           child: new Container(
+//             child: new Text(
+//               'NJI Media',
+//               style: CustomTextStyle.button(context),
+//             ),
+//           ),
+//         ),
+//       ],
+//     ),
+//   );
+// }
 
 class FirstScreen extends StatelessWidget {
   @override
@@ -85,7 +91,7 @@ class FirstScreen extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => SecondScreen()),
+              MaterialPageRoute(builder: (context) => SetupPage()),
             );
           }
         ),
@@ -94,13 +100,13 @@ class FirstScreen extends StatelessWidget {
   }
 }
 
-class SecondScreen extends StatelessWidget {
+class ErrorScreenWifi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-        'SOURCE',
+        'NO NETWORK',
           style: CustomTextStyle.label(context),
         ),
         backgroundColor: Colors.transparent,
@@ -113,11 +119,84 @@ class SecondScreen extends StatelessWidget {
             Navigator.pop(context);
           },
           child: Text(
-            'Back',
+            'Uh oh. Need Wifi.',
           style: CustomTextStyle.button(context),
             ),
         ),
       ),
     );
+  }
+}
+
+
+class SetupPage extends StatefulWidget {
+  @override
+  _SetupPageState createState() => new _SetupPageState();
+}
+
+class _SetupPageState extends State<SetupPage> {
+
+  var _connectionStatus = 'Unknown';
+  Connectivity connectivity;
+  StreamSubscription<ConnectivityResult> subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    connectivity = new Connectivity();
+    subscription =
+        connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      _connectionStatus = result.toString();
+      print(_connectionStatus);
+      if (result == ConnectivityResult.none) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ErrorScreenWifi()),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
+  }
+
+  Future getData() async {
+    http.Response response =
+        await http.get("https://jsonplaceholder.typicode.com/posts/");
+    if (response.statusCode == HttpStatus.ok) {
+      var result = jsonDecode(response.body);
+      return result;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(title: Text("Connectivity")),
+        body: new FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var mydata = snapshot.data;
+              return new ListView.builder(
+                itemBuilder: (context, i) => new ListTile(
+                      title: Text(
+                        mydata[i]['title'],
+                        style: CustomTextStyle.label(context),
+                      ),
+                      // subtitle: Text(mydata[i]['body']),
+                    ),
+                itemCount: mydata.length,
+              );
+            } else {
+              return Center(
+                child: new CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
   }
 }
